ad7e890042d71667a98996021171a7850a093bd6
lucky <takuto.yoshikai@gmail.com>
2021-05-12 19:45:11 +0900

    support binary

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::env;
 use std::io::Read;
 use std::path::Path;
+use std::fs::File;
+use std::{io, io::Write};
 use aes::Aes256;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
@@ -13,8 +15,15 @@ const IFACE: &str = "wlp3s0";
 
 const BASE_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-fn read_file(path: &str) -> String {
-    return fs::read_to_string(path).unwrap();
+fn read_file(path: &str) -> Option<Vec<u8>> {
+    let mut file = File::open(path).unwrap();
+    let metadata = fs::metadata(path).unwrap();
+    let mut buffer = vec![0; metadata.len() as usize];
+    match file.read(&mut buffer) {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    return Some(buffer);
 }
 fn gen_ascii_chars(size: usize) -> String {
     let mut rng = &mut rand::thread_rng();
@@ -38,29 +47,29 @@ fn get_key(if_name: &str) -> String {
     return get_macaddr(if_name) + "00000000000000";
 }
 
-fn encrypt(key: &str, data: &str) -> String {
+fn encrypt(key: &str, data: &[u8]) -> String {
     let iv_str = gen_ascii_chars(16);
     let iv = iv_str.as_bytes();
     let cipher = AesCbc::new_var(key.as_bytes(), iv).unwrap();
-    let ciphertext = cipher.encrypt_vec(data.as_bytes());
+    let ciphertext = cipher.encrypt_vec(data);
     let mut buffer = bytebuffer::ByteBuffer::from_bytes(iv);
     buffer.write_bytes(&ciphertext);
     base64::encode(buffer.to_bytes())
 }
 
-fn decrypt(key: &str, data: &str) -> String {
+fn decrypt(key: &str, data: &str) -> Vec<u8> {
     let bytes = base64::decode(data).unwrap();
     let cipher = AesCbc::new_var(key.as_bytes(), &bytes[0..16]).unwrap();
-    String::from_utf8(cipher.decrypt_vec(&bytes[16..]).unwrap()).unwrap()
+    cipher.decrypt_vec(&bytes[16..]).unwrap()
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
     let key = get_key(IFACE);
     if args.len() <= 1 {
-        print!("{}", decrypt(&key, &ENCRYPTED_TEXT));
+        io::stdout().write(&decrypt(&key, &ENCRYPTED_TEXT));
         return;
     }
     let target: &str = &args[1];
-    let text = read_file(target);
-    print!("{}", encrypt(&key, &text));
+    let data = read_file(target);
+    print!("{}", encrypt(&key, &data.unwrap()));
 }
